c225cb043d307001cb9eaa0c66e5c1adf687a21e
Kanaashi <douglaslopes1903@hotmail.com>
2024-05-20 15:59:23 -0300

    Atualização 2

--- src/App.js
+++ src/App.js
@@ -39,37 +39,43 @@ function App() {
   };
 
   return (
-    <div className="App">
-      <h1>Gerenciador de Serviços</h1>
-      <div className="form">
-        <input 
-          type="text" 
-          placeholder="Nome do Serviço" 
-          value={name} 
-          onChange={(e) => setName(e.target.value)} 
-        />
-        <input 
-          type="text" 
-          placeholder="Descrição do Serviço" 
-          value={description} 
-          onChange={(e) => setDescription(e.target.value)} 
-        />
-        {isEditing ? (
-          <button onClick={handleUpdateService}>Atualizar Serviço</button>
-        ) : (
-          <button onClick={handleAddService}>Adicionar Serviço</button>
-        )}
-      </div>
-      <div className="service-list">
-        <h2>Lista de Serviços</h2>
-        {services.map(service => (
-          <div key={service.id} className="service-item">
-            <h3>{service.name}</h3>
-            <p>{service.description}</p>
-            <button className="edit-button" onClick={() => handleEditService(service.id)}>Editar</button>
-            <button className="delete-button" onClick={() => handleDeleteService(service.id)}>Excluir</button>
+    <div className="container">
+      <div className="App">     
+        <div className="form-container">
+        <h1>Gerenciador de Serviços</h1>
+          <div className="form">
+            <input 
+              type="text" 
+              placeholder="Nome do Serviço" 
+              value={name} 
+              onChange={(e) => setName(e.target.value)} 
+            />
+            <input 
+              type="text" 
+              placeholder="Descrição do Serviço" 
+              value={description} 
+              onChange={(e) => setDescription(e.target.value)} 
+            />
+            {isEditing ? (
+              <button onClick={handleUpdateService}>Atualizar Serviço</button>
+            ) : (
+              <button onClick={handleAddService}>Adicionar Serviço</button>
+            )}
           </div>
-        ))}
+        </div>
+        <div className="service-list-container">
+          <h2>Lista de Serviços</h2>
+          <div className="service-list">
+            {services.map(service => (
+              <div key={service.id} className="service-item">
+                <h3>{service.name}</h3>
+                <p>{service.description}</p>
+                <button className="edit-button" onClick={() => handleEditService(service.id)}>Editar</button>
+                <button className="delete-button" onClick={() => handleDeleteService(service.id)}>Excluir</button>
+              </div>
+            ))}
+          </div>
+        </div>
       </div>
     </div>
   );
